7034deb9da4b4c8a4ef2e3bcae49d105c9391f66
yunfanshao <1504517387@qq.com>
2019-03-29 10:19:23 +0000

    Fixed two Rubocop offenses with the help of Srdjan

--- RB101/2_Lesson/my_mortgage.rb
+++ RB101/2_Lesson/my_mortgage.rb
@@ -1,4 +1,3 @@
-require "pry" # add this to use Pry
 require 'yaml'
 
 MESSAGES = YAML.load_file('my_mortgage_message.yml')
@@ -7,31 +6,52 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def valid_number(num)
+def valid_number?(num)
   integer?(num) || float?(num)
 end
 
-def integer?(input)
-  input.to_i.to_s == input
+def integer?(num)
+  num.to_i.to_s == num
 end
 
-def float?(input)
-  input.to_f.to_s == input
+def float?(num)
+  num.to_f.to_s == num
 end
 
-def annual_tranfer_monthly_interest_rate(input)
-  input.to_f / 12
+def annual_tranfer_monthly_rate(annual_rate)
+  annual_rate.to_f / 12
 end
 
-def years_tranfer_months_load_dur(years)
+def years_tranfer_months_loan_dur(years)
   years.to_i * 12
 end
 
-def calculator_result_monthly_payment(loan_amount, monthly_interest_rate, load_dur_in_months)
+def calculator_monthly_payment(loan_amount, monthly_rate, loan_dur_in_months)
   a = loan_amount.to_f
-  j = monthly_interest_rate / 100
-  n = load_dur_in_months
+  j = monthly_rate / 100
+  n = loan_dur_in_months
   a * (j / (1 - (1 + j)**-n))
+end
+
+def valid_loan_amount?(loan)
+  valid_number?(loan) && loan.to_f > 0
+end
+
+def valid_annual_rate?(annual_rate)
+  valid_number?(annual_rate) && annual_rate.to_f > 0
+end
+
+def valid_loan_dur_in_years?(loan_dur_in_years)
+  integer?(loan_dur_in_years) && loan_dur_in_years.to_i > 0
+end
+
+def zero_load_amount?(loan)
+  loan == '0'
+end
+
+def display_zero_loan_amount
+  prompt MESSAGES['good_luck']
+  prompt MESSAGES['loan_amount_input']
 end
 
 def recalculate?
@@ -48,45 +68,54 @@ end
 prompt('welcome')
 
 loop do
-  total_loan_amount = nil
+  loan = nil
   prompt MESSAGES['loan_amount_input']
   loop do
-    total_loan_amount = gets.chomp
-    break if valid_number(total_loan_amount) && total_loan_amount.to_f > 0
-    if total_loan_amount == '0'
-      prompt MESSAGES['good_luck']
-      prompt MESSAGES['loan_amount_input']
+    loan = gets.chomp
+    break if valid_loan_amount?(loan)
+    if zero_load_amount?(loan)
+      display_zero_loan_amount()
     else
       prompt MESSAGES['need_posti_num']
     end
   end
 
-  annual_per_rate = nil
-  prompt MESSAGES['annual_per_rate_input']
+  annual_rate = nil
+  prompt MESSAGES['annual_rate_input']
   loop do
-    annual_per_rate = gets.chomp
-    break if valid_number(annual_per_rate)
-    prompt MESSAGES['enter_again']
+    annual_rate = gets.chomp
+    break if valid_annual_rate?(annual_rate)
+    if annual_rate.to_f <= 0
+      prompt MESSAGES['postive_num']
+    end
   end
 
   loan_dur_in_years = nil
   prompt MESSAGES['loan_dur_in_years_input']
   loop do
     loan_dur_in_years = gets.chomp
-    break if integer?(loan_dur_in_years) && loan_dur_in_years.to_i > 0
-    prompt MESSAGES['enter_again']
+    break if valid_loan_dur_in_years?(loan_dur_in_years)
+    if loan_dur_in_years.to_f < 0
+      prompt MESSAGES['postive_num']
+    elsif float?(loan_dur_in_years)
+      prompt MESSAGES['integer_num']
+    else
+      prompt MESSAGES['zero_input_hint']
+    end
   end
 
-  monthly_interest_rate = annual_tranfer_monthly_interest_rate(annual_per_rate)
-  load_dur_in_months = years_tranfer_months_load_dur(loan_dur_in_years)
+  monthly_rate = annual_tranfer_monthly_rate(annual_rate)
+  loan_dur_in_months = years_tranfer_months_loan_dur(loan_dur_in_years)
 
-  if annual_per_rate == '0'
-    result_monthly_payment = total_loan_amount.to_f / load_dur_in_months
+  if annual_rate == '0'
+    monthly_payment = loan.to_f / loan_dur_in_months
   else
-    result_monthly_payment = calculator_result_monthly_payment(total_loan_amount, monthly_interest_rate, load_dur_in_months)
+    monthly_payment = calculator_monthly_payment(loan,
+                                                 monthly_rate,
+                                                 loan_dur_in_months)
   end
 
-  prompt("result_monthly_payment: $#{format('%0.2f', result_monthly_payment)}")
+  prompt("monthly_payment: $#{format('%0.2f', monthly_payment)}")
 
   break unless recalculate?
 end
